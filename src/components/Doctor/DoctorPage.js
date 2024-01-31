aa9b21beadc63d33f6f6268c84c9d9fc35fb80e7
Harsh <harshsantoshi05@gmail.com>
2024-02-01 00:35:30 +0530

    Added search functionality to Doctor page

--- src/components/Doctor/DoctorPage.js
+++ src/components/Doctor/DoctorPage.js
@@ -1,10 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import DoctorCard from './DoctorCard.js';
-import {jwtDecode} from 'jwt-decode';
+import { jwtDecode } from 'jwt-decode';
+import styled from 'styled-components';
+
+const Input = styled('input')`
+font-size : 16px;
+padding:5px;
+border-bottom:4px solid black;
+border-radius:10px;
+color:darkblue;
+`
 const DoctorPage = () => {
   const [allDoctors, setAllDoctors] = useState([]);
   const [doctorId, setDoctorId] = useState("");
   const [patientId, setPatientId] = useState("");
+  const [filteredDoctors, setFilteredDoctors] = useState([]);
+  const [query, setQuery] = useState(""); // Added query state
+
   useEffect(() => {
     // Fetch the token from localStorage
     const token = localStorage.getItem('token');
@@ -12,14 +24,10 @@ const DoctorPage = () => {
 
     if (token) {
       const decodedToken = jwtDecode(token);
-      // console.log(decodedToken);
-      if(role == "doctor"){
+      if (role === "doctor") {
         const doctorIdFromToken = decodedToken.doctor.id;
-
         setDoctorId(doctorIdFromToken);
-        // console.log(doctorId);
-      }
-      else{
+      } else {
         const patientIdFromToken = decodedToken.patient.id;
         setPatientId(patientIdFromToken);
       }
@@ -40,15 +48,34 @@ const DoctorPage = () => {
     fetchData();
   }, []);
 
+  const handleSearch = (value) => {
+    // Filter doctors based on the search query
+    setQuery(value);
+    const filtered = allDoctors.filter((doctor) =>
+      doctor.doctorName.toLowerCase().includes(value.toLowerCase())
+    );
+    setFilteredDoctors(filtered);
+  };
+
   return (
-    <div >
+    <div>
       <h1>Doctor Page</h1>
-      <div style={{ height: "auto", display: "flex" , flexWrap: 'wrap', justifyContent:'space-evenly' }}>
-        {allDoctors.map((doctor, index) => (
+      <div style={{position:"absolute" , right : '20px' , top:'92px'}}>
+      <Input
+        placeholder='Search Doctor'
+        value={query}
+        onChange={(e) => handleSearch(e.target.value)}
+      />
+      </div>
 
-          <DoctorCard key={index} props={{ name: doctor.doctorName, specialization: doctor.specialization, fees: doctor.fees, id: doctor._id, rating: "4.6", totalRatings :'10' , patientId : patientId }} />
-
-        ))}
+      <div style={{ height: "auto", display: "flex", flexWrap: 'wrap', justifyContent: 'space-evenly' }}>
+        {filteredDoctors.length
+          ? filteredDoctors.map((doctor, index) => (
+            <DoctorCard key={index} props={{ name: doctor.doctorName, specialization: doctor.specialization, fees: doctor.fees, id: doctor._id, rating: "4.6", totalRatings: '10', patientId: patientId }} />
+          ))
+          : allDoctors.map((doctor, index) => (
+            <DoctorCard key={index} props={{ name: doctor.doctorName, specialization: doctor.specialization, fees: doctor.fees, id: doctor._id, rating: "4.6", totalRatings: '10', patientId: patientId }} />
+          ))}
       </div>
     </div>
   );
