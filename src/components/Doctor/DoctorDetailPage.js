11219e586d23365084ccbdaf7cf58f4df8aeba6f
Harsh <harshsantoshi05@gmail.com>
2024-02-01 21:45:24 +0530

    Updated Profile Section of Doctor

--- src/components/Doctor/DoctorDetailPage.js
+++ src/components/Doctor/DoctorDetailPage.js
@@ -1,17 +1,94 @@
 import React from 'react'
 import { useLocation } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import styled from 'styled-components';
+const Image = styled('img')`
+height : 250px;
+width : 200px;
+border-radius : 10px;
+`
+const Specialization = styled('div')({
+  backgroundColor: '#CCF0F3',
+  // width:"content",
+  color: 'blue',
+  padding: '2px 6px',
+  textAlign:"center",
+  fontWeight: '700',
+  borderRadius: '4px',
+});
+const Content = styled('div')`
+margin:10px;
+max-width:500px;
+text-align:left;
+padding:20px;
+display:flex;
+flex-direction : column;
+align-items:start;
+height : 230px;
+overflow-y: auto;
+&::-webkit-scrollbar {
+  width: 8px;  
+}
+`
+const Name = styled('div')`
+font-size : 25px;
+font-weight : bold;
+`
+
+const StarContainer = styled('div')({
+  display: 'flex',
+  alignItems: 'center',
+  gap: '5px',
+});
+const Fee = styled('span')`
+
+`
+
+const TagLine = styled('div')`
+font-size : 16px;
+`
+const Star = styled('i')`
+  color: gold;
+`;
+
 
 const DoctorDetailPage = () => {
     const location = useLocation();
     const doctorID = location.state?.doctorID;
     const doctorInformation = location.state?.info;
   return (
-    <div>
-       
-      <h2>Detail Page</h2>
-      {/* {console.log(doctorInformation)} */}
-      Name : {doctorInformation.name}
-      
+    <div style={{ maxWidth:"90%" , margin:"0 auto"}}>
+      <div style={{display:"flex" , justifyContent:"center"  , padding : "30px 5px"}}>
+        <div >
+          <Image src= "https://www.summit-urgentcare.com/wp-content/uploads/2014/12/Dollarphotoclub_69741928.jpg" />
+        </div>
+        <Content>
+        <Specialization>Surgeon</Specialization>
+        <Name> Harsh Santoshi </Name>
+        <StarContainer>
+        <Star className="fa-solid fa-star" />
+
+          <span style={{ display: 'flex', alignItems: 'center', gap: '5px', fontSize: '16px' }}> 4.5
+          </span>
+          <span style={{ fontSize: '15px', color: 'grey', fontWeight: '400' }}>
+            (100) 
+          </span>
+        </StarContainer>
+        <Fee> <span style={{fontSize:"18px" , fontWeight:"600"}}>Consultation Fees </span>  : $40</Fee>
+        <TagLine>Lorem ipsum is fsa aspw jfaw paiw asap asfep ansjd panv aint wf f;akhf ;akheg ;ehg bfhagbf kagefjsbjkrgwrlgf wfbkal felugf jakbgk jah fjabefjbaejfbelug a aejf kkaf aefg uafa fkjaee fjkae fajkaef fhah ahf kf aewfuhfiuw whqi f whf iwh f ehih erq rqhi rh f sfh whe eqhuq t iewhf sjf wei hiwer hf ahfeiw oqirj fha ioae kereghie w hiogFKHAYSNF JHSE EF fhsefh jg ajhats fkhg kjsnatoshi hcla ds  thhha tt sfjal</TagLine>
+        </Content>
+
+      </div>
+      <hr/>
+
+      <div>
+      <div>
+        About
+      </div>
+      <div>
+        Feedback
+      </div>
+      </div>
     </div>
   )
 }
--- src/components/Doctor/DoctorProfile.js
+++ src/components/Doctor/DoctorProfile.js
@@ -1,20 +1,58 @@
 import React from 'react';
 import "./doctorprofile.css"
+import styled from 'styled-components';
+import Footer from "../Footer/Footer.js"
+const CollegeName = styled('span')`
+font-Weight : 600;
+`
+const WorkPlace = styled('span') `
+font-Weight : 600;
+`
+const Degree = styled('div')`
+font-size : 14px;
+color : grey;
+`
+const Year = styled('div')`
+font-size : 14px;
+color:grey
+`
+const Star = styled('i')`
+  color: gold;
+  margin-right : 5px;
+`;
+const Hospital = styled('i') `
+color:red;
+`
+const Role = styled('div')`
+font-weight : 700;
+`
+
+
 
 const DoctorProfile = () => {
   return (
-    <>
+    <div className="bg">
       <div className='profile-container'>
         <div className='intro'>
           <div className='dr_image'>
             <img src='profile.png' alt="" />
+            <button className='btn btn-primary mt-3'>Change profile Image</button>
           </div>
           <div className='d-flex flex-wrap'>
             <div className='col-md-9 details'>
               <h3 className='name'>Harsh</h3>
               <p className='specialization'>Neurologist</p>
               <p className='location'>Delhi, India</p>
-              <p className='count'>Treatment of 500+ Patients</p>
+              <div style={{display:'flex' , fontSize:"16px"}}>
+              <p className='count'>Treatment of 500+ Patients </p>
+              |
+              <p style={{margin:"0 10px" }}>
+              <Star className="fa-solid fa-star" />
+               4.5(10)</p>
+              </div>
+              <div style={{fontSize : "16px"}}>
+                10+ years of Experience
+              </div>
             </div>
             <div className='col-md-3'>
               <i className="fa-solid fa-hospital icon"></i>
@@ -23,34 +61,53 @@ const DoctorProfile = () => {
           </div>
         </div>
         <div className='cnt'>
-            <h3 className='title'>About</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab alias qui at nisi nostrum dolorum neque! Cum non beatae ipsa?</p>
+          <h3 className='title'>About</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab alias qui at nisi nostrum dolorum neque! Cum non be fwiej fhaioe eij qo ejqf dkn kagj eafhua fe fjfue fajke biuf bjibf jbfejk aeuhga kj dug qihwif q hrieh wf sihf a fuhga jghuire gwuefhqiohf nj dvia hefe gioah gknjvka jkfka v ih ef wehfoihef ohefjafgafeh ewh qnjn qfatae ipsa?</p>
         </div>
         <div className='cnt'>
-            <h3 className='title'>Services</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab alias qui at nisi nostrum dolorum neque! Cum non beatae ipsa?</p>
-        </div>
-        <div className='cnt'>
-            <h3 className='title'>Education</h3>
-            <span>MBBS</span>
-            <hr />
-        </div>
-        <div className='cnt'>
-            <h3 className='title'>Experience</h3>
-            <div className='work'>
-                <p className='workat'>Worked At</p> 
-                <span>AIIMS Delhi</span><span>2 years</span>
+          <h3 className='title'>Experience</h3>
+          <div style={{ display: "flex" ,margin:"10px 0" }}>
+            <div style={{ margin: "auto 2px" }}><Hospital className="fa-solid fa-hospital icon" /></div>
+            <div>
+              <WorkPlace>Grand Oak Hospital</WorkPlace>
+              <Role>Surgeon</Role>
+              <Year>(Dec 2000 - Present)</Year>
             </div>
+          </div>
+          <hr/>
+          <div style={{ display: "flex" ,margin:"10px 0" }}>
+            <div style={{ margin: "auto 2px" }}><Hospital className="fa-solid fa-hospital icon" /></div>
+            <div>
+              <WorkPlace>Grand Oak Hospital</WorkPlace>
+              <Role>Surgeon</Role>
+              <Year>(Dec 2000 - Present)</Year>
+            </div>
+          </div>
         </div>
         <div className='cnt'>
-            <h3 className='title'>Information</h3>
-            <div className='info'>
-              <p>Rating</p>
-              <span>4.5(100)</span>
+          <h3 className='title'>Education</h3>
+          <div style={{ display: "flex" ,margin:"10px 0" }}>
+            <div style={{ margin: "auto 2px" }}><Hospital className="fa-solid fa-hospital icon" /></div>
+            <div>
+              <CollegeName>AIIMS Delhi</CollegeName>
+              <Degree>MBBS</Degree>
+              <Year>(Dec 2000 - Jul 2005)</Year>
             </div>
+           
+          </div>
+          <hr/>
+          <div style={{ display: "flex" }}>
+            <div style={{ margin: "auto 2px" }}><Hospital className="fa-solid fa-hospital icon" /></div>
+            <div>
+              <CollegeName>AIIMS Delhi</CollegeName>
+              <Degree>MBBS</Degree>
+              <Year>(Dec 2000 - Jul 2005)</Year>
+            </div>
+          </div>
         </div>
       </div>
-    </>
+      <Footer/>
+    </div>
   )
 }
 
