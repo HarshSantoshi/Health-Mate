1602097fa2231fe5f6c34685cb882102763d7534
Harsh <hk3711750@gmail.com>
2024-05-30 14:54:50 +0530

    Update Modal

--- src/components/Doctor/DoctorProfile.js
+++ src/components/Doctor/DoctorProfile.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect,useState,useRef } from 'react';
+import React, { useContext, useEffect, useState, useRef } from 'react';
 import "./doctorprofile.css"
 import styled from 'styled-components';
 import Footer from "../Footer/Footer.js";
@@ -38,22 +38,25 @@ const style = {
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 800,
-  height: '70%',
-  overflow: 'scroll',
-  backgroundColor: 'background.paper',
-  borderRadius: '15px',
-  // border: '2px solid #000',
-  // boxShadow: '24px', 
+  width: '90%',
+  maxWidth: 600,
+  bgcolor: 'background.paper',
+  maxHeight: '90vh',
+  overflowY: 'scroll',
+  boxShadow: 24,
+  p: 4,
+  borderRadius: '8px',
+  display: 'flex',
+  flexDirection: 'column',
+  position: 'relative',
   '&::-webkit-scrollbar': {
     width: '0px',
   },
-  p: '16px', 
 };
 
 
 const DoctorProfile = () => {
-  const { doctor, experience, education, fetchData,updatedoctorProfile,updatedoctor,Addexp ,Deleteexp,Addedu,Deleteedu} = useContext(doctorcontext);
+  const { doctor, experience, education, fetchData, updatedoctorProfile, updatedoctor, Addexp, Deleteexp, Addedu, Deleteedu } = useContext(doctorcontext);
   //Info start
   const [info, setInfo] = useState({
     doctorName: "",
@@ -63,11 +66,11 @@ const DoctorProfile = () => {
     fees: "",
     phoneNo: "",
     about: "",
-    doctorImage : ""
+    doctorImage: ""
   });
   const presetKey = "healthmate";
   const cloud_name = "dgarsqfvl";
-  const [image , setImage] = useState("profile.png");
+  const [image, setImage] = useState("profile.png");
 
   const [openabout, setOpenabout] = useState(false);
 
@@ -90,7 +93,7 @@ const DoctorProfile = () => {
       fees: doctor.fees || "",
       phoneNo: doctor.phoneNo || "",
       about: doctor.about || "",
-      doctorImage : doctor.doctorImage||""
+      doctorImage: doctor.doctorImage || ""
     });
     setOpenabout(true);
   };
@@ -105,7 +108,7 @@ const DoctorProfile = () => {
       fees: doctor.fees || "",
       phoneNo: doctor.phoneNo || "",
       about: doctor.about || "",
-      doctorImage : doctor.doctorImage||""
+      doctorImage: doctor.doctorImage || ""
     });
   };
 
@@ -126,7 +129,7 @@ const DoctorProfile = () => {
     e.preventDefault();
     ref.current.click();
     Addexp(exp.hospital, exp.service, handledate(exp.startdate), handledate(exp.enddate))
-    setexp({hospital: "",service: "",startdate: "",enddate: ""})
+    setexp({ hospital: "", service: "", startdate: "", enddate: "" })
   };
   //experience end
   //education start
@@ -140,15 +143,15 @@ const DoctorProfile = () => {
   const handleeduchange = (e) => {
     setedu({ ...edu, [e.target.name]: e.target.value })
   }
-  const uploadPicture = async() => {
-  
-    
+  const uploadPicture = async () => {
+
+
     const data = new FormData();
 
     data.append("file", image);
     data.append("upload_preset", presetKey);
-    data.append("cloud_name" , cloud_name)
-  
+    data.append("cloud_name", cloud_name)
+
     fetch(`https://api.cloudinary.com/v1_1/${cloud_name}/image/upload`, {
       // mode : "no-cors",
       method: "POST",
@@ -156,34 +159,42 @@ const DoctorProfile = () => {
     })
       .then(response => response.json())
       .then(data => {
-        
+
         updatedoctorProfile(data.url)
-        
-        
+
+
       })
       .catch(error => console.error("Error uploading image:", error));
-      
+
   };
-  
+
   const handleEdu = (e) => {
     e.preventDefault();
     refedu.current.click();
-    Addedu(edu.institution, edu.speciality,handledate(edu.startdate), handledate(edu.enddate));
-    setedu({institution: "",speciality: "",startdate: "",enddate: ""})
+    Addedu(edu.institution, edu.speciality, handledate(edu.startdate), handledate(edu.enddate));
+    setedu({ institution: "", speciality: "", startdate: "", enddate: "" })
   };
   //education end
-  const month = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
-  const handledate = (date)=>{
+  const month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  const handledate = (date) => {
     const [year, mon] = date.split('-');
-    return month[parseInt(mon)-1] + '-' + year ;
+    return month[parseInt(mon) - 1] + '-' + year;
   }
   //Modal start
   const [openexp, setOpenexp] = React.useState(false);
   const [openedu, setOpenedu] = React.useState(false);
   const handleOpenexp = () => setOpenexp(true);
-  const handleCloseexp = () => setOpenexp(false);
+  const handleCloseexp = () => {
+    ref.current.click();
+    setexp({ hospital: "", service: "", startdate: "", enddate: "" })
+    setOpenexp(false);
+  }
   const handleOpenedu = () => setOpenedu(true);
-  const handleCloseedu = () => setOpenedu(false);
+  const handleCloseedu = () => {
+    refedu.current.click();
+    setedu({ institution: "", speciality: "", startdate: "", enddate: "" })
+    setOpenedu(false);
+  }
   //Modal end
 
   useEffect(() => {
@@ -207,12 +218,12 @@ const DoctorProfile = () => {
       <div className='profile-container'>
         <div className='intro'>
           <div style={{ textAlign: "right" }} onClick={handleOpenabout}><i className="fa-solid fa-pen Add"></i></div>
-          
+
           <div className='dr_image'>
-          <img src={doctor.doctorImage? doctor.doctorImage : 'profile.png'} alt="" />
+            <img src={doctor.doctorImage ? doctor.doctorImage : 'profile.png'} alt="" />
           </div>
-          <input type = "file" name = "image" onChange={(e)=>setImage(e.target.files[0])}></input>
-           <button className='btn btn-primary mt-3' onClick={uploadPicture}>Change profile Image</button>
+          <input type="file" name="image" onChange={(e) => setImage(e.target.files[0])}></input>
+          <button className='btn btn-primary mt-3' onClick={uploadPicture}>Change profile Image</button>
 
           <div className='d-flex flex-wrap'>
             <div className='col-md-9 details'>
@@ -252,7 +263,7 @@ const DoctorProfile = () => {
               <i className="fa-solid fa-plus Add"></i>
             </div>
           </div>
-          {experience.length !== 0 && experience.map((exp,i) => (
+          {experience.length !== 0 && experience.map((exp, i) => (
             <div key={i}>
               <div style={{ display: "flex", margin: "10px 0" }}>
                 <div style={{ margin: "auto 2px" }}><Hospital className="fa-solid fa-hospital icon" /></div>
@@ -273,7 +284,7 @@ const DoctorProfile = () => {
               <i className="fa-solid fa-plus Add"></i>
             </div>
           </div>
-          {education.length !== 0 && education.map((edu,i) => (
+          {education.length !== 0 && education.map((edu, i) => (
             <div key={i}>
               <div style={{ display: "flex", margin: "10px 0" }}>
                 <div style={{ margin: "auto 2px" }}><Hospital className="fa-solid fa-hospital icon" /></div>
@@ -297,21 +308,24 @@ const DoctorProfile = () => {
         aria-describedby="keep-mounted-modal-description"
       >
         <Box sx={style}>
-          <h3 className='title' style={{ color: "black" }}>Experience</h3>
-          {experience.length !== 0 && experience.map((exp,i) => (
+          <i
+            className="fa-solid fa-xmark close-icon"
+            aria-label="close"
+            onClick={handleCloseexp}
+          />
+          <h3 className='title-exp'>Experience</h3>
+          {experience.length !== 0 && experience.map((exp, i) => (
             <div key={i}>
               <div className='d-flex justify-content-between'>
-                <div style={{ display: "flex", margin: "10px 0" }}>
-                  <div style={{ margin: "auto 2px" }}><Hospital className="fa-solid fa-hospital icon" /></div>
+                <div className='experience-item'>
+                  <Hospital className="fa-solid fa-hospital icon" />
                   <div>
-                    <WorkPlace>{exp.hospital}</WorkPlace>
-                    <Role>{exp.service}</Role>
-                    <Year>({exp.startdate} - {exp.enddate})</Year>
+                    <div className="work-place">{exp.hospital}</div>
+                    <div className="role">{exp.service}</div>
+                    <div className="year">({exp.startdate} - {exp.enddate})</div>
                   </div>
                 </div>
-                <div >
-                  <i className="fa-solid fa-trash remove" onClick={()=>Deleteexp(exp._id)}></i>
-                </div>
+                <i className="fa-solid fa-trash remove" onClick={() => Deleteexp(exp._id)}></i>
               </div>
               <hr />
             </div>
@@ -328,24 +342,22 @@ const DoctorProfile = () => {
                   <form className="row g-3" onSubmit={handleExp}>
                     <div className="col-md-6">
                       <label htmlFor="hospital" className="form-label">Hospital Name</label>
-                      <input type="text" className="form-control" id="hospital" name="hospital" value={exp.hospital} onChange={handleexpchange} required/>
+                      <input type="text" className="form-control" id="hospital" name="hospital" value={exp.hospital} onChange={handleexpchange} required />
                     </div>
                     <div className="col-md-6">
                       <label htmlFor="service" className="form-label">Service</label>
-                      <input className="form-control" type='text' id="service" value={exp.service} name="service" onChange={handleexpchange} required/>
+                      <input className="form-control" type='text' id="service" value={exp.service} name="service" onChange={handleexpchange} required />
                     </div>
                     <div className="col-md-6">
                       <label htmlFor="startdate" className="form-label">Started In</label>
-                      <input className="form-control" type='month' id="startdate" value={exp.startdate} name="startdate" onChange={handleexpchange} required/>
+                      <input className="form-control" type='month' id="startdate" value={exp.startdate} name="startdate" onChange={handleexpchange} required />
                     </div>
                     <div className="col-md-6">
-                      <label htmlFor="enddate" className="form-label">Complete  (if pursue leave)</label>
-                      <input className="form-control" type='month' id="enddate" value={exp.enddate} name="enddate" onChange={handleexpchange} required/>
+                      <label htmlFor="enddate" className="form-label">Complete (if pursue leave)</label>
+                      <input className="form-control" type='month' id="enddate" value={exp.enddate} name="enddate" onChange={handleexpchange} required />
                     </div>
                     <div className='col-12 text-center'>
-                      <div className="text-center">
-                        <button type="submit" className="btn btn-success me-2">Save</button>
-                      </div>
+                      <button type="submit" className="btn btn-success me-2">Save</button>
                     </div>
                   </form>
                 </div>
@@ -361,9 +373,22 @@ const DoctorProfile = () => {
         aria-labelledby="keep-mounted-modal-title"
         aria-describedby="keep-mounted-modal-description"
       >
+
         <Box sx={style}>
+          <i className="fa-solid fa-xmark"
+            style={{
+              position: 'absolute',
+              right: 8,
+              top: 8,
+              color: 'grey.500',
+              fontSize: "20px",
+              cursor: "pointer"
+            }}
+            aria-label="close"
+            onClick={handleCloseedu} >
+          </i>
           <h3 className='title' style={{ color: "black" }}>Education</h3>
-          {education.length !== 0 && education.map((edu,i) => (
+          {education.length !== 0 && education.map((edu, i) => (
             <div key={i}>
               <div className='d-flex justify-content-between'>
                 <div style={{ display: "flex", margin: "10px 0" }}>
@@ -375,7 +400,7 @@ const DoctorProfile = () => {
                   </div>
                 </div>
                 <div>
-                  <i className="fa-solid fa-trash remove" onClick={()=>Deleteedu(edu._id)}></i>
+                  <i className="fa-solid fa-trash remove" onClick={() => Deleteedu(edu._id)}></i>
                 </div>
               </div>
               <hr />
@@ -393,19 +418,19 @@ const DoctorProfile = () => {
                   <form className="row g-3" onSubmit={handleEdu}>
                     <div className="col-md-6">
                       <label htmlFor="institution" className="form-label">Institution Name</label>
-                      <input type="text" className="form-control" id="institution" name="institution" value={edu.institution} onChange={handleeduchange} required/>
+                      <input type="text" className="form-control" id="institution" name="institution" value={edu.institution} onChange={handleeduchange} required />
                     </div>
                     <div className="col-md-6">
                       <label htmlFor="speciality" className="form-label">Specilization</label>
-                      <input className="form-control" type='text' id="speciality" value={edu.speciality} name="speciality" onChange={handleeduchange} required/>
+                      <input className="form-control" type='text' id="speciality" value={edu.speciality} name="speciality" onChange={handleeduchange} required />
                     </div>
                     <div className="col-md-6">
                       <label htmlFor="startdate" className="form-label">Started In</label>
-                      <input className="form-control" type='month' id="startdate" value={edu.startdate} name="startdate" onChange={handleeduchange} required/>
+                      <input className="form-control" type='month' id="startdate" value={edu.startdate} name="startdate" onChange={handleeduchange} required />
                     </div>
                     <div className="col-md-6">
                       <label htmlFor="enddate" className="form-label">Complete  (if pursue leave)</label>
-                      <input className="form-control" type='month' id="enddate" value={edu.enddate} name="enddate" onChange={handleeduchange} required/>
+                      <input className="form-control" type='month' id="enddate" value={edu.enddate} name="enddate" onChange={handleeduchange} required />
                     </div>
                     <div className='col-12 text-center'>
                       <div className="text-center">
@@ -427,40 +452,50 @@ const DoctorProfile = () => {
         aria-describedby="keep-mounted-modal-description"
       >
         <Box sx={style}>
-          <h3 className='title' style={{ color: "black" }}>Edit Info</h3>
+          <i className="fa-solid fa-xmark"
+            style={{
+              position: 'absolute',
+              right: 8,
+              top: 8,
+              color: 'grey.500',
+              fontSize: "20px",
+              cursor: "pointer"
+            }}
+            aria-label="close"
+            onClick={handleCloseabout} >
+          </i>
+          <h3 className='title' style={{ color: "black", textAlign: 'center' }}>Edit Info</h3>
           <form className="row g-3" onSubmit={handleSubmit}>
-            <div className="col-md-6">
+            <div className="col-md-6 col-sm-12">
               <label htmlFor="doctorName" className="form-label">Name</label>
-              <input type="text" className="form-control" id="doctorName" name="doctorName" value={info.doctorName} readOnly/>
+              <input type="text" className="form-control" id="doctorName" name="doctorName" value={info.doctorName} readOnly />
             </div>
-            <div className="col-md-6">
+            <div className="col-md-6 col-sm-12">
               <label htmlFor="specialization" className="form-label">Specialization</label>
-              <input className="form-control" type='text' id="specialization" value={info.specialization} name="specialization" onChange={onchange}/>
+              <input className="form-control" type='text' id="specialization" value={info.specialization} name="specialization" onChange={onchange} />
             </div>
-            <div className="col-md-6">
+            <div className="col-md-6 col-sm-12">
               <label htmlFor="experienceYrs" className="form-label">Experience</label>
-              <input className="form-control" type='Number' id="experienceYrs" value={info.experienceYrs} name="experienceYrs" onChange={onchange}/>
+              <input className="form-control" type='Number' id="experienceYrs" value={info.experienceYrs} name="experienceYrs" onChange={onchange} />
             </div>
-            <div className="col-md-6">
+            <div className="col-md-6 col-sm-12">
               <label htmlFor="currentlyserving" className="form-label">Currently Serving</label>
-              <input className="form-control" type='text' id="currentlyserving" value={info.currentlyserving} name="currentlyserving" onChange={onchange}/>
+              <input className="form-control" type='text' id="currentlyserving" value={info.currentlyserving} name="currentlyserving" onChange={onchange} />
             </div>
-            <div className="col-md-6">
+            <div className="col-md-6 col-sm-12">
               <label htmlFor="fees" className="form-label">Fees</label>
-              <input className="form-control" type='Number' id="fees" value={info.fees} name="fees" onChange={onchange}/>
+              <input className="form-control" type='Number' id="fees" value={info.fees} name="fees" onChange={onchange} />
             </div>
-            <div className="col-md-6">
-              <label htmlFor="phoneNo" className="form-label">PhoneNo</label>
-              <input className="form-control" type='Number' id="phoneNo" value={info.phoneNo} name="phoneNo" onChange={onchange}/>
+            <div className="col-md-6 col-sm-12">
+              <label htmlFor="phoneNo" className="form-label">Phone No</label>
+              <input className="form-control" type='Number' id="phoneNo" value={info.phoneNo} name="phoneNo" onChange={onchange} />
             </div>
             <div className="col-12">
               <label htmlFor="about" className="form-label">About</label>
               <textarea className="form-control" id="about" value={info.about} name="about" rows="3" onChange={onchange}></textarea>
             </div>
             <div className='col-12 text-center'>
-              <div className="text-center">
-                <button type="submit" className="btn btn-success me-2">Save</button>
-              </div>
+              <button type="submit" className="btn btn-success me-2">Save</button>
             </div>
           </form>
         </Box>
